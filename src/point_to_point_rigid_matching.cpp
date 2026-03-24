2a6f9d03f2ec5315d21733aea7e4b38b744c6bf6
Alon Sardas <alonsardas@gmail.com>
2026-03-24 23:55:18 +0200

    Adding point-to-point matching and icp_single_iteration implementation

--- src/point_to_point_rigid_matching.cpp
+++ src/point_to_point_rigid_matching.cpp
@@ -1,14 +1,29 @@
 #include "point_to_point_rigid_matching.h"
 #include "closest_rotation.h"
+#include <Eigen/Dense>
+#include <iostream>
 
-void point_to_point_rigid_matching(
-  const Eigen::MatrixXd & X,
-  const Eigen::MatrixXd & P,
-  Eigen::Matrix3d & R,
-  Eigen::RowVector3d & t)
-{
-  // Replace with your code
-  R = Eigen::Matrix3d::Identity();
-  t = Eigen::RowVector3d::Zero();
+void point_to_point_rigid_matching(const Eigen::MatrixXd &X,
+                                   const Eigen::MatrixXd &P, Eigen::Matrix3d &R,
+                                   Eigen::RowVector3d &t) {
+  Eigen::RowVector3d p_bar = P.colwise().mean();
+  Eigen::RowVector3d x_bar = X.colwise().mean();
+  Eigen::MatrixXd X_bar = X.rowwise() - x_bar;
+  Eigen::MatrixXd P_bar = P.rowwise() - p_bar;
+  assert(X_bar.rows() == X.rows() && X_bar.cols() == 3 &&
+         "X_bar has wrong size");
+  assert(P_bar.rows() == X.rows() && P_bar.cols() == 3 &&
+         "P_bar has wrong size");
+  Eigen::Matrix3d M = P_bar.transpose() * X_bar;
+  closest_rotation(M, R);
+  // std::cout << "M R similarity " << (M.array() * R.array()).sum() <<
+  // std::endl; std::cout << "M Identity similarity "
+  //        << (M.array() * Eigen::Matrix3d::Identity().array()).sum()
+  //        << std::endl;
+  // std::cout << "M " << M << std::endl;
+  // std::cout << "R " << R << std::endl;
+  // std::cout << "R^T * R:\n" << R.transpose() * R << std::endl;
+  // std::cout << "det(R): " << R.determinant() << std::endl;
+  // R = Eigen::Matrix3d::Identity();
+  t = p_bar - x_bar * R.transpose();
 }
-
---
+++ tests/test_rigid_matching.cpp
@@ -0,0 +1,87 @@
+#include "hausdorff_lower_bound.h"
+#include "icp_single_iteration.h"
+#include "point_mesh_distance.h"
+#include "point_to_plane_rigid_matching.h"
+#include "point_to_point_rigid_matching.h"
+#include "random_points_on_mesh.h"
+#include "test_meshes.h"
+#include <cmath>
+#include <gtest/gtest.h>
+
+// These are very simple tests mainly to make sure the functions don't crush
+
+double compute_distance(const Eigen::MatrixXd &X, const Eigen::MatrixXd &P);
+void run_rigid_matching_test(const Eigen::MatrixXd &VX,
+                             const Eigen::MatrixXi &FX,
+                             const Eigen::MatrixXd &VY,
+                             const Eigen::MatrixXi &FY, const int num_samples,
+                             const ICPMethod method);
+
+TEST(PointToPointRigidMatching, TestCubeToCircleRigidMatching) {
+  Eigen::MatrixXd VX;
+  Eigen::MatrixXi FX;
+  Eigen::MatrixXd VY;
+  Eigen::MatrixXi FY;
+
+  generate_circle_mesh(VX, FX, Eigen::RowVector3d(0, -2, 0), 1.0);
+  generate_cube_mesh(VY, FY, Eigen::RowVector3d(6, 0, 0), 4.0);
+
+  int samples = 200;
+  run_rigid_matching_test(VX, FX, VY, FY, samples,
+                          ICPMethod::ICP_METHOD_POINT_TO_POINT);
+}
+
+TEST(PointToPlaneRigidMatching, TestCubeToCircleRigidMatching) {
+  Eigen::MatrixXd VX;
+  Eigen::MatrixXi FX;
+  Eigen::MatrixXd VY;
+  Eigen::MatrixXi FY;
+
+  generate_circle_mesh(VX, FX, Eigen::RowVector3d(0, -12, 0), 1.0);
+  generate_cube_mesh(VY, FY, Eigen::RowVector3d(-3, 0, 0), 4.0);
+
+  int samples = 200;
+  run_rigid_matching_test(VX, FX, VY, FY, samples,
+                          ICPMethod::ICP_METHOD_POINT_TO_PLANE);
+}
+
+// This is almost duplicate of icp_single_iteration
+void run_rigid_matching_test(const Eigen::MatrixXd &VX,
+                             const Eigen::MatrixXi &FX,
+                             const Eigen::MatrixXd &VY,
+                             const Eigen::MatrixXi &FY, const int num_samples,
+                             const ICPMethod method) {
+  Eigen::MatrixXd X;
+  Eigen::VectorXd D;
+  Eigen::MatrixXd P, N;
+  random_points_on_mesh(num_samples, VX, FX, X);
+  point_mesh_distance(X, VY, FY, D, P, N);
+
+  // averaging the distances using L2 norm
+  double initial_distance = D.norm();
+
+  Eigen::Matrix3d R;
+  Eigen::RowVector3d t;
+
+  if (method == ICPMethod::ICP_METHOD_POINT_TO_POINT) {
+    point_to_point_rigid_matching(X, P, R, t);
+  } else if (method == ICPMethod::ICP_METHOD_POINT_TO_PLANE) {
+    point_to_plane_rigid_matching(X, P, N, R, t);
+  } else {
+    assert(false && "Got unknown ICPMethod, shouldn't reach here");
+  }
+
+  Eigen::MatrixXd X_transformed = (R * X.transpose()).transpose();
+  X_transformed = X_transformed.rowwise() + t;
+
+  double final_distance = compute_distance(X_transformed, P);
+
+  EXPECT_LT(final_distance, initial_distance)
+      << "rigid matching did not decrease the distance for method "
+      << "point-to-"
+      << (method == ICP_METHOD_POINT_TO_PLANE ? "plane" : "point");
+}
+
+double compute_distance(const Eigen::MatrixXd &X, const Eigen::MatrixXd &P) {
+  return (X - P).norm();
+}
